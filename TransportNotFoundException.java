d95a372b8a4c4a2fee7b34e9fd9b0710f4ce3d41
balalakshmideepthi <deepthibommisetty121@gmail.com>
2024-04-20 21:35:43 +0530

    .

--- TransportNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.javaproject.Transport;
-
-public class TransportNotFoundException extends RuntimeException {
-    public TransportNotFoundException(String message) {
-        super(message);
-    }
-}
--- TransportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.javaproject.Transport;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransportRepository extends CrudRepository<Transport, Integer> {
-    Long countById(Integer id);
-}
